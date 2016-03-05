8b1b60502783e89215de853544e08e9df05f98a5
Planeshifter <pburckhardt@outlook.com>
2016-03-05 12:24:09 -0500

    Add tests

---
+++ test/test.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// MODULES //
+
+var tape = require( 'tape' );
+var abs = require( 'math-abs' );
+var zeta = require( './../lib' );
+var linspace = require( 'compute-linspace' );
+
+
+// FIXTURES //
+
+var output = JSON.parse( require( './fixtures/output.json' ).program_message );
+var data = output.x;
+var expected = output.expected;
+
+
+// TESTS //
+
+tape( 'main export is a function', function test( t ) {
+	t.ok( typeof zeta === 'function', 'main export is a function' );
+	t.end();
+});
+
+tape( 'if provided `NaN`, the function returns `NaN`', function test( t ) {
+	var v = zeta( NaN );
+	t.ok( v !== v, 'returns NaN when provided a NaN' );
+	t.end();
+});
+
+tape( 'the function evaluates the Riemann zeta function', function test( t ) {
+	var delta;
+	var tol;
+	var v;
+	var i;
+
+	for ( i = 0; i < data.length; i++ ) {
+		v = zeta( data[ i ] );
+		delta = abs( v - expected[ i ] );
+		tol = 1e-14 * Math.max( 1, abs( v ), abs( expected[ i ]) );
+		t.ok( delta <= tol, 'within tolerance. x: ' + data[ i ] + '. Value: ' + v + '. Expected: ' + expected[ i ] + '. Tolerance: ' + tol + '.' );
+	}
+	t.end();
+});
+
+tape( 'the function evaluates the Riemann zeta function for very small values', function test( t ) {
+	var v = zeta( 1e-10 );
+	t.ok( v === -0.5000000000918938 );
+	t.end();
+});
+
+tape( 'if evaluated at pole `1`, the function returns `NaN`', function test( t ) {
+	var v = zeta( 1 );
+	t.ok( v !== v, 'returns NaN when provided 1' );
+	t.end();
+});
+
+tape( 'the function evaluates returns `1` for input values greater or equal than `63`', function test( t ) {
+	var s = linspace( 63, 100, 200 );
+	var i;
+	for ( i = 0; i < s.length; i++ ) {
+		var v = zeta( s[ i ] );
+		t.ok( v === 1 );
+	}
+	t.end();
+});
