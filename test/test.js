62fac4507c1319936906ea68478e54540fee87bd
kgryte <kgryte@gmail.com>
2016-03-10 13:51:53 -0800

    Add special value tests

--- test/test.js
+++ test/test.js
@@ -77,3 +77,61 @@ tape( 'the function evaluates returns `1` for all input values greater or equal 
 
 	t.end();
 });
+
+tape( 'if provided `0` (special value), the function returns `-0.5`', function test( t ) {
+	var v = zeta( 0 );
+	t.equal( v, -0.5, 'returns -0.5' );
+	t.end();
+});
+
+tape( 'if provided `-1` (special value), the function returns `-1/12`', function test( t ) {
+	var v = zeta( -1 );
+	t.equal( v, -1/12, 'returns -1/12' );
+	t.end();
+});
+
+tape( 'if provided `-13` (special value), the function returns `-1/12`', function test( t ) {
+	var v = zeta( -13 );
+	t.equal( v, -1/12, 'returns -1/12' );
+	t.end();
+});
+
+tape( 'if provided `4` (special value), the function returns `~1.0823`', function test( t ) {
+	var v = zeta( 4 );
+
+	// https://oeis.org/A0013662
+	t.equal( v, 1.082323233711138191516003696541167, 'returns ~1.0823' );
+	t.end();
+});
+
+tape( 'if provided `3` (special value), the function returns `~1.202`', function test( t ) {
+	var v = zeta( 3 );
+
+	// https://oeis.org/A002117
+	t.equal( v, 1.2020569031595942853997, 'returns ~1.202' );
+	t.end();
+});
+
+tape( 'if provided `2` (special value), the function returns `~1.645`', function test( t ) {
+	var v = zeta( 2 );
+
+	// https://oeis.org/A013661
+	t.equal( v, 1.6449340668482264364724151666460251892189499012067984377355582293700074704032, 'returns ~1.645' );
+	t.end();
+});
+
+tape( 'if provided `3/2` (special value), the function returns `~2.612`', function test( t ) {
+	var v = zeta( 3/2 );
+
+	// https://oeis.org/A078434
+	t.equal( v, 2.61237534868548834334856756792407163057080065240006340757332824881492776768827286099624386812631195238297, 'returns ~2.612' );
+	t.end();
+});
+
+tape( 'if provided `1/2` (special value), the function returns `~-1.46`', function test( t ) {
+	var v = zeta( 1/2 );
+
+	// https://oeis.org/A059750
+	t.equal( v, -1.4603545088095868128894991525152980124672293310125814905428860878, 'returns ~-1.46' );
+	t.end();
+});
