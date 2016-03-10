d8a2dc944fc72c455649e91524307b3cd4c23ab1
kgryte <kgryte@gmail.com>
2016-03-09 20:56:56 -0800

    Clean up tests

--- test/test.js
+++ test/test.js
@@ -18,7 +18,7 @@ var expected = output.expected;
 // TESTS //
 
 tape( 'main export is a function', function test( t ) {
-	t.ok( typeof zeta === 'function', 'main export is a function' );
+	t.equal( typeof zeta, 'function', 'main export is a function' );
 	t.end();
 });
 
@@ -45,7 +45,7 @@ tape( 'the function evaluates the Riemann zeta function', function test( t ) {
 
 tape( 'the function evaluates the Riemann zeta function for very small values', function test( t ) {
 	var v = zeta( 1e-10 );
-	t.ok( v === -0.5000000000918938 );
+	t.equal( v, -0.5000000000918938 );
 	t.end();
 });
 
@@ -57,10 +57,11 @@ tape( 'if evaluated at pole `1`, the function returns `NaN`', function test( t )
 
 tape( 'the function evaluates returns `1` for input values greater or equal than `63`', function test( t ) {
 	var s = linspace( 63, 100, 200 );
+	var v;
 	var i;
 	for ( i = 0; i < s.length; i++ ) {
-		var v = zeta( s[ i ] );
-		t.ok( v === 1 );
+		v = zeta( s[ i ] );
+		t.equal( v, 1, 'returns 1 when provided '+s[i] );
 	}
 	t.end();
 });
